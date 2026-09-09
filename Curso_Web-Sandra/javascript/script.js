1ccc6dcd3e394d24fdc99e355cbd60d0dfeb3a53
sandraMartina <sandrateletor7@gmail.com>
2026-09-09 14:42:31 +0200

    moreJava

--- Curso_Web-Sandra/javascript/script.js
+++ Curso_Web-Sandra/javascript/script.js
@@ -33,3 +33,88 @@ console.log(today);
 console.log("Hello world hope you miss.");
 
 
+//CONSOLE.LOG
+
+
+//CONSOLE.INFO("CONSOLE.INFO")      INFORMATIVE MESSAGES
+//CONSOLE.WARN                      Warning  
+//CONSOLE.ERROR                     ERROR
+
+
+
+
+//MESSAGES GROUP
+console.group("Mensajes for you");
+console.log("Mensaje 1")
+console.log("Mensaje 2")
+console.groupEnd();
+
+
+//if i want this group not to be opend 
+//the user has to opend it
+//MESSAGES GROUP
+console.groupCollapsed("Mensajes for you");
+console.log("Mensaje 1")
+console.log("Mensaje 2")
+console.groupEnd();
+
+let hello = document.getElementById("hello");
+
+
+//Constantes TODOMAYUSCULA Y _GUIONBAJO
+BIRTHDAY_DAY = 2000;
+const BIRTHDAY = 2000;
+
+console.log(BIRTHDAY);
+
+
+//VARIABLE we have to use the camelCase
+
+let nombre = "Sandra";
+nombre = "Sandra Tele"
+let edad = 29;
+let mayorEdad = true;
+
+
+console.log(nombre);
+
+
+let contador = 0;
+console.log(contador);
+
+
+
+contador = contador + 1;
+console.log(contador);
+
+
+
+contador = contador + 1;
+console.log(contador);
+
+
+// Mathematic operations
+
+let number1 = 20;
+let number2 = 30;
+
+
+// suma
+resultado = number1 + number2;
+console.log(resultado);
+
+// resta
+ resultado = number1 - number2;
+console.log("El resultado de la resta es " + resultado);
+
+// multiplicacion
+ resultado = number1 * number2;
+console.log("El resultado de la multiplicacion es " + resultado);
+
+// division
+ resultado = number1 / number2;
+console.log("El resultado de la division es " + resultado);
+
+
+//directamente desde el el console.log
+console.log(number1 * number2);
