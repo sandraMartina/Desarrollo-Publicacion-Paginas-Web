a84a574a0cf807aa6f94c4620afec720d5661d61
sandraMartina <sandrateletor7@gmail.com>
2026-09-09 12:45:29 +0200

    javascrip

---
+++ Curso_Web-Sandra/javascript/script.js
@@ -0,0 +1,35 @@
+
+
+// this include all page
+// alert("Hello World")
+
+
+//Comment several lines of code
+/*
+console.log("Hello World hope you miss me")
+console.log("Hello World hope you miss me")
+console.log("Hello World hope you miss me")
+*/
+
+
+
+
+
+//creating an alert
+alert("Welcome to class people")
+
+//on the console we're creatinga message on the console
+console.log("Sandra");
+
+
+//creating a variable
+let today = new Date();
+
+//on the console we're putting today's date
+console.log(today);
+
+
+//Small text
+console.log("Hello world hope you miss.");
+
+
